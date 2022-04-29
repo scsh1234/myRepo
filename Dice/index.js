f0b18adf1f2e026f6bfd531a98db8e919a622f01
scsh1234 <103987116+scsh1234@users.noreply.github.com>
2022-04-29 20:04:07 +0530

    Update index.js

--- Dice/index.js
+++ Dice/index.js
@@ -1,7 +1,7 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;//1-6;
 
 var mystring="images/dice"+randomNumber1+".png";
-// document.querySelector("img").setAttribute("src",mystring);
+// document.querySelectorAll("img").setAttribute("src",mystring);
 document.getElementById("img1").setAttribute("src",mystring);
 // document.getElementsByClassName("img1").setAttribute("src",mystring);
 
@@ -12,13 +12,13 @@ document.getElementById("img2").setAttribute("src",mystring2);
 
 if(randomNumber1>randomNumber2)
 {
-    document.getElementById("h32").innerHTML="PLAYER 1 WINS !";
+    document.querySelector("h1").innerHTML="PLAYER 1 WINS !";
 }
 else if(randomNumber2>randomNumber1)
 {
-    document.getElementById("h32").innerHTML="PLAYER 2 WINS !"
+    document.querySelector("h1").innerHTML="PLAYER 2 WINS !"
 }
 else
 {
-    document.getElementById("h32").innerHTML="DRAW";
+    document.querySelector("h1").innerHTML="DRAW";
 }
