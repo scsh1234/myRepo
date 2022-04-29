d255d9ff8f9ad18a15aabaef7c1739e3d114b335
scsh1234 <103987116+scsh1234@users.noreply.github.com>
2022-04-29 19:51:56 +0530

    Add files via upload

---
+++ Dice/index.js
@@ -0,0 +1,24 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;//1-6;
+
+var mystring="images/dice"+randomNumber1+".png";
+// document.querySelector("img").setAttribute("src",mystring);
+document.getElementById("img1").setAttribute("src",mystring);
+// document.getElementsByClassName("img1").setAttribute("src",mystring);
+
+var randomNumber2=Math.floor(Math.random()*6)+1;//1-6;
+
+var mystring2="images/dice"+randomNumber2+".png";
+document.getElementById("img2").setAttribute("src",mystring2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.getElementById("h32").innerHTML="PLAYER 1 WINS !";
+}
+else if(randomNumber2>randomNumber1)
+{
+    document.getElementById("h32").innerHTML="PLAYER 2 WINS !"
+}
+else
+{
+    document.getElementById("h32").innerHTML="DRAW";
+}
